c1e2acb516d891cc72df860b4c25bc47f95bc1d6
Aramizu <rafael.gomes@picpay.com>
2021-09-06 16:17:05 -0300

    bottom nav fragments

--- presentation/src/main/java/br/dev/aramizu/tmdb/home/HomeActivity.kt
+++ presentation/src/main/java/br/dev/aramizu/tmdb/home/HomeActivity.kt
@@ -2,14 +2,16 @@ package br.dev.aramizu.tmdb.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import br.dev.aramizu.domain.features.movie.list.GetMovieListUseCase
+import br.dev.aramizu.tmdb.R
 import br.dev.aramizu.tmdb.databinding.ActivityMainBinding
 import br.dev.aramizu.tmdb.home.viewmodel.ActionState
 import br.dev.aramizu.tmdb.home.viewmodel.HomeViewModel
 import br.dev.aramizu.tmdb.home.viewmodel.ViewState
-import org.koin.android.ext.android.inject
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeActivity : AppCompatActivity() {
@@ -17,22 +19,55 @@ class HomeActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
     private val viewModel: HomeViewModel by viewModel()
 
+    private val nowPlayingMoviesFragment = NowPlayingMoviesFragment()
+    private val searchMoviesFragment = SearchMoviesFragment()
+    private val favouritesMoviesFragment = FavouritesMoviesFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         binding?.let { setContentView(it.root) }
 
         setUpObservation()
+        setUpBottomNavigation()
+        switchFragment(nowPlayingMoviesFragment)
+    }
+
+    private fun setUpBottomNavigation() {
+        binding?.bottomNavigation?.let {
+            it.setOnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.nav_now_playing -> {
+                        switchFragment(nowPlayingMoviesFragment)
+                        true
+                    }
+                    R.id.nav_search -> {
+                        switchFragment(searchMoviesFragment)
+                        true
+                    }
+                    R.id.nav_favourites -> {
+                        switchFragment(favouritesMoviesFragment)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations( android.R.anim.fade_in, android.R.anim.fade_out )
+            .replace(R.id.fragment_container, fragment, fragment.tag)
+            .commit()
     }
 
     private fun setUpObservation() {
         viewModel.viewState.observe(this, Observer { state ->
             when (state) {
                 is ViewState.ShowContent -> {
-                    binding?.textViewGreeting?.text = state.content
-                    binding?.textViewGreeting?.setOnClickListener {
-                        viewModel.action()
-                    }
+
                 }
             }
         })
@@ -43,5 +78,10 @@ class HomeActivity : AppCompatActivity() {
                 else -> TODO()
             }
         })
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
